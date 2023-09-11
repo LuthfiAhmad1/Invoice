388e6f9c9774de03a53cdf6481f98512bce3536e
LuthfiAhmad1 <144640413+LuthfiAhmad1@users.noreply.github.com>
2023-09-11 09:34:00 +0700

    Create Luassegitiga.cpp

---
+++ Luassegitiga.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main( ){
+      int a,t;
+       cout << "MENGHITUNG LUAS SEGITIGA \n";
+      cout<<"masukan angka pertama: ";
+      cin>>a;
+      cout<<"masukan angka kedua ";
+      cin>> t;
+     cout <<"hasil luas: " << 0.5*a*t << endl;
+       return 0;
